d91bc0310d394a09210b4f48aa7a3f6b971f0f39
rohit-0807 <rohitjeerankalagi07@gmil.com>
2025-04-26 11:01:42 +0530

    updated

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from 'react-router-dom';
+import './Login.css';
 
 const Login = () => {
   const [form, setForm] = useState({email:'',password:''});
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './Signup.css'; 
+
 
 const Signup = () => {
   const [form, setForm] = useState({ name: '', email: '', password: '' });
